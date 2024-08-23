ba3ee9e7ff5373d53155b77d4ed848bde7d21b52
Emilio Navarro <sainfuw@outlook.com>
2024-08-23 19:54:48 -0400

    change node to cloudflare for deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
+import cloudflare from '@astrojs/cloudflare'
 import mdx from '@astrojs/mdx'
 import sitemap from '@astrojs/sitemap'
 import { defineConfig } from 'astro/config'
-
-import node from '@astrojs/node'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
   integrations: [mdx(), sitemap()],
   output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
+  adapter: cloudflare(),
 })
