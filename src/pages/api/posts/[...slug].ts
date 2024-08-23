cd20f44111b55e311ea04ed78e976dcb810a8040
Emilio Navarro <sainfuw@outlook.com>
2024-08-23 19:50:44 -0400

    crud api posts

--- src/pages/api/posts/[...slug].ts
+++ src/pages/api/posts/[...slug].ts
@@ -1,9 +1,9 @@
-import type { APIRoute } from 'astro'
+import { type APIRoute } from 'astro'
 import { getEntry } from 'astro:content'
 
 export const prerender = false
 
-export const GET: APIRoute = async ({ params, request }) => {
+export const GET: APIRoute = async ({ params, request, cookies }) => {
   const { slug } = params
 
   const post = await getEntry('blog', slug)
@@ -12,7 +12,30 @@ export const GET: APIRoute = async ({ params, request }) => {
     return new Response('Not found', { status: 404 })
   }
 
+  cookies.set('test', 'hello', { path: '/api/posts' })
+
   return new Response(JSON.stringify(post), {
     headers: { 'Content-Type': 'application/json' },
   })
+}
+
+export const POST: APIRoute = async ({ params, request, cookies }) => {
+  console.log({ cookies })
+  const body = await request.json()
+  return new Response(JSON.stringify(body))
+}
+
+export const PUT: APIRoute = async ({ params, request }) => {
+  const body = await request.json()
+  return new Response(JSON.stringify(body))
+}
+
+export const PATCH: APIRoute = async ({ params, request }) => {
+  const body = await request.json()
+  return new Response(JSON.stringify(body))
+}
+
+export const DELETE: APIRoute = async ({ params, request }) => {
+  const body = await request.json()
+  return new Response(JSON.stringify(body))
 }
