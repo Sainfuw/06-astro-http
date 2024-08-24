4c8558989735e251b026ddab14f1fe05f001a799
Emilio Navarro <sainfuw@outlook.com>
2024-08-24 17:50:22 -0400

    astro db seed updated

--- db/seed.ts
+++ db/seed.ts
@@ -2,13 +2,12 @@ import { Client, db } from 'astro:db'
 
 // https://astro.build/db/seed
 export default async function seed() {
-  // TODO
   await db.insert(Client).values([
-    { id: 1, name: 'John', age: 30, isActive: true },
-    { id: 2, name: 'Jane', age: 25, isActive: false },
-    { id: 3, name: 'Bob', age: 40, isActive: true },
-    { id: 4, name: 'Alice', age: 35, isActive: false },
-    { id: 5, name: 'Eve', age: 20, isActive: true },
+    { name: 'John', age: 30, isActive: true },
+    { name: 'Jane', age: 25, isActive: false },
+    { name: 'Bob', age: 40, isActive: true },
+    { name: 'Alice', age: 35, isActive: false },
+    { name: 'Eve', age: 20, isActive: true },
   ])
 
   console.log('Seeded!')
